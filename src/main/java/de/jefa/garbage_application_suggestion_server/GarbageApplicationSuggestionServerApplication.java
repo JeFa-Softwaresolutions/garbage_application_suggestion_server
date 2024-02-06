b7e076a92ad4d06d86eac8f3157d6c7376e8cd6b
BuildTools <unconfigured@null.spigotmc.org>
2024-02-06 17:20:22 +0100

    Initial commit

---
+++ src/main/java/de/jefa/garbage_application_suggestion_server/GarbageApplicationSuggestionServerApplication.java
@@ -0,0 +1,13 @@
+package de.jefa.garbage_application_suggestion_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageApplicationSuggestionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarbageApplicationSuggestionServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/jefa/garbage_application_suggestion_server/GarbageApplicationSuggestionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jefa.garbage_application_suggestion_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageApplicationSuggestionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
